e9ec9470c9e3fbc2fab169d660a056c10a4c1b3d
Ao Xu <aoxu@usc.edu>
2023-11-06 00:56:41 -0800

    predict.sh

---
+++ rf_predict.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --account=katritch_502
+#SBATCH --partition=main
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=16G
+#SBATCH --time=4:00:00
+
+module purge
+eval "$(conda shell.bash hook)"
+# module load conda
+conda activate molpal
+python predict.py
